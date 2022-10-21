38155c08e824d21a4da493df44a9e994878fe73e
Thiago Carvalho <thiagoabcarvalho@gmail.com>
2022-10-21 16:29:54 -0300

    ajustando caminho das imagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,10 @@
-import { subscribeToHellfireClub } from "./firebase/hellfire-club.js"
-
-(function main(){
-const txtName = document.getElementById('txtName')
-const txtEmail = document.getElementById('txtEmail')
-const txtLevel = document.getElementById('txtLevel')
-const txtCharacter = document.getElementById('txtCharacter')
-
-const btnSubscribe = document.getElementById('btnSubscribe')
-
-btnSubscribe.addEventListener('click', async () => {
-    const subscription = {
-        name: txtName.value,
-        email: txtEmail.value,
-        level: txtLevel.value,
-        character: txtCharacter.value
-    }
-    //salvar no banco de dados!
-    //subscribetoHellfireClub(subscription)
-
-    const subscriptionid = await subscribeToHellfireClub(subscription) 
-    alert('Inscrição ${id} adcionada com sucesso!')
-})
-
-})()
-
-
 //mudança do tema light/dark
-/* 
-function switchTheme() {
+
+//const themeDarkLignt = document.querySelector("#switch-theme-button")
+const addTheme = document.querySelector("#switch-theme-button")
+ function switchTheme() {
     document.body.classList.toggle('dark-theme');
     document.body.classList.toggle('light-theme');
 }
- */
+
 
